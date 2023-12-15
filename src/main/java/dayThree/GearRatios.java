e718bc5901660f38a7e4abeb69a80169ef38f2b4
sarinaamin <s.amin@elsevier.com>
2023-12-15 13:36:48 +0000

    day 3 part 1 refactoring

--- src/main/java/dayThree/GearRatios.java
+++ src/main/java/dayThree/GearRatios.java
@@ -56,6 +56,8 @@ public class GearRatios {
                 for (int i = startIndex; i < endIndex; i++) {
                     sb.setCharAt(i, '.');
                 }
+
+                row = sb.toString();
             }
         }
 
